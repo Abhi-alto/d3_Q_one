abcd1817f8a1bd247abe2046f150ff020066d60c
unknown <abhisheksrivastava9719@gmail.com>
2022-06-14 11:12:57 +0530

    UC1 written

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation program");
+Random ran = new Random();
+int r=ran.Next(0,2);
+if (r == 1)
+{
+    Console.WriteLine("Prsent");
+}
+else
+{
+    Console.WriteLine("Absent");  
+}
