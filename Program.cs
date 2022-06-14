8be5b0ca87b39c70625658313dc9d9e863c2747d
unknown <abhisheksrivastava9719@gmail.com>
2022-06-14 11:19:39 +0530

    Added UC2 file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ int r=ran.Next(0,2);
 if (r == 1)
 {
     Console.WriteLine("Prsent");
+    int WaPhr = 20;
+    int F_DaHr = 8;
+    int da_wa = WaPhr * F_DaHr;
+    Console.WriteLine("Daily wage of the employee is = " + da_wa);
 }
 else
 {
